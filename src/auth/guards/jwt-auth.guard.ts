8910f1bb87b18eeb438b6d2e9924138276c0b10e
Tina Homayouni <t.homayouni@vardastapp.com>
2024-01-02 01:46:21 +0330

    old jwt-auth.guard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,21 +1,6 @@
-// jwt-auth.guard.ts
-import {
-  Injectable,
-  ExecutionContext,
-  UnauthorizedException,
-} from '@nestjs/common';
+// src/auth/guards/jwt-auth.guard.ts
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
-  }
-
-  handleRequest(err, user, info) {
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-    return user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
